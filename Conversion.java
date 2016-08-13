5607367995e7f7c17e1c1d6a4ad4717c5c332733
anjuit <anjuramanbe@gmail.com>
2016-08-13 11:50:27 +0530

    Create Conversion.java

---
+++ Conversion.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Conversion
+{
+public Static void main(String arg[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the String");
+String s=sc.next();
+int n=Integer.parseInt(s);
+System.out.println(n);
+}
+}
+
