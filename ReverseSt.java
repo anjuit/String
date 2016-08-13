c87fad829bf5c1c31159a861502ebd8634e7942a
anjuit <anjuramanbe@gmail.com>
2016-08-13 20:11:04 +0530

    Create ReverseSt.java

---
+++ ReverseSt.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Reverse {
+	public static void main(String arg[])
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the String");
+		StringBuffer s=new StringBuffer(sc.next());
+		s=s.reverse();
+		System.out.println(s);
+		String s1=s.toString();
+		  s1=s1.replaceAll("[aeiou]","");
+		  System.out.println("output "+s1);
+		
+	}
